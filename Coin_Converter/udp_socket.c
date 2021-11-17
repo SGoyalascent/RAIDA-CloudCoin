209a658fb53d82d5ced6a92730b4e4ab866dc1d8
SGoyalascent <shubham@ascentspark.com>
2021-11-17 19:50:57 +0530

    Updated

--- Coin_Converter/udp_socket.c
+++ Coin_Converter/udp_socket.c
@@ -302,7 +302,7 @@ void execute_coin_converter(unsigned int packet_len){
     fscanf(myfile, "Host = %255s Database = %255s Username = %255s Password = %255s listenport = %d encryption_key = %255s mode = %255s", Host_ip, Database_name,
                                                   Username, User_password, &listen_port, Encryption_key, Mode);
     fclose(myfile);
-    printf("Host = %s\n Database = %s\n Username = %s\n Password = %s\n listenport = %d\n encryption_key = %s\n mode = %s\n", Host_ip, Database_name, Username, User_password, &listen_port, Encryption_key, Mode);
+    printf("Host = %s\n Database = %s\n Username = %s\n Password = %s\n listenport = %d\n encryption_key = %s\n mode = %s\n", Host_ip, Database_name, Username, User_password, listen_port, Encryption_key, Mode);
 
 
 // Initialize a connection to the Database---------------------
